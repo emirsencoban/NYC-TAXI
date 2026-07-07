aab01a60433b2427e93bf8ab46408f2060a7266c
Emir Sencoban <emirsencoban@icloud.com>
2026-07-08 00:56:29 +0300

    Update .gitignore and enhance main.py to include SQLite database integration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import pyarrow
+import sqlite3
 
 #--Defining URL --
 url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet"
@@ -22,8 +23,8 @@ else:
 
 #-- Reading the Parquet file into a DataFrame --
 df = pd.read_parquet("25.01.parquet")
-print("Shape of the DataFrame:", df.shape)
-print("DataFrame Columns:", df.columns)
+""" print("Shape of the DataFrame:", df.shape)
+print("DataFrame Columns:", df.columns) """
 
 #-- Transforming the Data --
 df_clean = df[(df["passenger_count"] > 0) & (df["passenger_count"] < 7)]
@@ -32,5 +33,12 @@ df_clean = df_clean[df_clean["fare_amount"] > 0]
 df_clean = df_clean[df_clean["total_amount"] > 0]
 df_clean = df_clean.dropna(subset=["passenger_count", "trip_distance", "fare_amount", "total_amount", "tpep_pickup_datetime"])
 
-print("Shape of the cleaned DataFrame:", df_clean.shape)
-print("Number of rows dropped:", df.shape[0] - df_clean.shape[0])
+""" print("Shape of the cleaned DataFrame:", df_clean.shape)
+print("Number of rows dropped:", df.shape[0] - df_clean.shape[0]) """
+
+#-- LOAD TO SQL --
+conn = sqlite3.connect("nyc_taxi.db")
+print("Connected to SQLite database.")
+df_clean.to_sql("yellow_tripdata_2025_01", conn, if_exists="replace", index=False)
+conn.close()
+print("Data loaded into SQLite database successfully.")
