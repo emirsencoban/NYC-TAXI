ca9c6a6ab64b7b877d5a08a0cc8ea2dc6694b517
Emir Sencoban <emirsencoban@icloud.com>
2026-07-07 23:49:18 +0300

    Fix import statement for requests and add functionality to download and read Parquet files

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-import request
+import requests
 import pandas as pd
 import pyarrow
 
 #--Defining URL --
 url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet"
 
-response = request.get(url, stream=True)
+response = requests.get(url, stream=True)
 
 if response.status_code == 200:
     print("File downloaded successfully.")
-    ...
-
-
+    
+    #-- Saving the file as 25.01.parquet --
+    with open("25.01.parquet", "wb") as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                file.write(chunk)
+    print("File saved as 25.01.parquet")
+    
 else:
     print(f"Failed to download file. Status code: {response.status_code}")
+
+#-- Reading the Parquet file into a DataFrame --
+df = pd.read_parquet("25.01.parquet")
+print("Shape of the DataFrame:", df.shape)
+print("DataFrame Columns:", df.columns)
