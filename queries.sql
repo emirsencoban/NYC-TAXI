af570d77e1fa3e41529d1a17f55c2941819494cc
Emir Sencoban <emirsencoban@icloud.com>
2026-07-08 22:22:18 +0300

    Add SQL query to retrieve top 5 drop-off locations by average total amount for trips longer than 5 miles

--- queries.sql
+++ queries.sql
@@ -4,3 +4,11 @@ FROM yellow_tripdata_2025_01
 GROUP BY  PULocationID 
 ORDER BY average_tip DESC
 LIMIT 5;
+
+-- Top 5 drop-off locations with the highest average total amount for trips longer than 5 miles
+SELECT DOLocationID , AVG(total_amount) AS average_amount
+FROM yellow_tripdata_2025_01
+WHERE trip_distance > 5
+GROUP BY DOLocationID  
+ORDER BY average_amount DESC
+LIMIT 5;
